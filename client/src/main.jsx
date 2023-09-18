4184acc450fc7d220395ce65c5a7985eeba0ddb7
Paul Yi <pauljsyi@gmail.com>
2023-09-18 05:17:16 -0600

    new routes config

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,20 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
 
+// vercel.json routes config
+// {
+//   "routes": [
+//     { "src": "/", "dest": "/" },
+//     { "src": "/menu", "dest": "/" },
+//     { "src": "/merch", "dest": "/" },
+//     { "src": "/upload", "dest": "/" },
+//     { "src": "/cart", "dest": "/" },
+//     { "src": "/register", "dest": "/" },
+//     { "src": "/login", "dest": "/" },
+//     { "src": "/cancel", "dest": "/" }
+//   ]
+// }
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
